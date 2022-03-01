b242e4d7085f9e050fd7b7d94f011821e41a82e5
Praveen B <praveen.b@exotel.in>
2022-03-01 23:18:35 +0530

    Updated frontend

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// const apiurl = "http://localhost:5000/todos"
+// const apiurl = "http://localhost:8000/todos"
 
 const apiurl = "https://praveenb-todoapp.herokuapp.com/todos"
 
@@ -10,7 +10,8 @@ function markcompleted(id) {
         xhttp.onreadystatechange = function() {
             if (this.readyState == 4 && this.status == 200) {
                 console.log(this.responseText)
-                window.location.reload();
+                // window.location.reload();
+                getTodos();
             }
             else {
                 console.log(this.responseText)
@@ -30,10 +31,8 @@ function deleteTodo(id) {
         xhttp.onreadystatechange = function() {
             if (this.readyState == 4 && this.status == 200) {
                 console.log(this.responseText)
-                window.location.reload();
-            }
-            else {
-                console.log(this.responseText)
+                // window.location.reload();
+                getTodos();
             }
         };
         xhttp.open("DELETE", apiurl + "/" + id, true);
@@ -56,6 +55,15 @@ function getTodos() {
                 todosObj = data
             }
 
+            // console.log(todosObj)
+
+            let activeTodosElm = document.getElementById("activeTodos");
+            let completedTodosElm = document.getElementById("completedTodos");
+
+            activeTodosElm.innerHTML = ``;
+            completedTodosElm.innerHTML = ``;
+            
+
             let html = "";
             if(todosObj["activetodos"])
             todosObj["activetodos"].reverse().forEach(function (element, index) {
@@ -71,14 +79,15 @@ function getTodos() {
                 </div>
                     `;
             });
-            let activeTodosElm = document.getElementById("activeTodos");
-            if (todosObj['activetodos'].length != 0) {
+            
+            if (todosObj['activetodos'] != null && todosObj['activetodos'].length != 0) {
                 activeTodosElm.innerHTML = html;
             } else {
                 activeTodosElm.innerHTML = `<p class="lead px-1">No todos found!</p>`;
             }
 
             html = "";
+            if(todosObj["completedtodos"])
             todosObj["completedtodos"].reverse().forEach(function (element, index) {
                 html += `
                 <div class="todo">
@@ -89,8 +98,8 @@ function getTodos() {
                 </div>
                     `;
             });
-            let completedTodosElm = document.getElementById("completedTodos");
-            if (todosObj['completedtodos'].length != 0) {
+            
+            if (todosObj['completedtodos'] != null && todosObj['completedtodos'].length != 0) {
                 completedTodosElm.innerHTML = html;
             } else {
                 completedTodosElm.innerHTML = `<p class="lead px-1">No todos found!</p>`;
@@ -111,10 +120,12 @@ function IsEmpty(value) {
 
 var exampleModal = document.getElementById('exampleModal')
 exampleModal.addEventListener('show.bs.modal', function (event) {
+  event.stopPropagation();
   // Button that triggered the modal
   var button = event.relatedTarget
 
   var id = button.getAttribute('id')
+  console.log(id);
 
   if(id === "createbtn") {
     document.getElementById("exampleModalLabel").innerHTML = `Create Todo`
@@ -144,12 +155,12 @@ exampleModal.addEventListener('show.bs.modal', function (event) {
         var jsonString= JSON.stringify(todojson);
 
         if(id === "createbtn") {
-            console.log("Inside Create"+ button.getAttribute('id'));
+            console.log(button.getAttribute('id'));
             var createrequest = new XMLHttpRequest();
             createrequest.onreadystatechange = function() {
                 if (this.readyState == 4 && this.status == 200) {
-                    console.log(this.responseText)
-                    window.location.reload();
+                    console.log(this.responseText);
+                    window.location = window.location
                 }
             };
             createrequest.open("POST", apiurl, true);
@@ -161,8 +172,8 @@ exampleModal.addEventListener('show.bs.modal', function (event) {
             var updaterequest = new XMLHttpRequest();
             updaterequest.onreadystatechange = function() {
                 if (this.readyState == 4 && this.status == 200) {
-                    console.log(this.responseText)
-                    window.location.reload();
+                    console.log(this.responseText);
+
                 }
             };
             updaterequest.open("PUT", apiurl + "/" + button.getAttribute('id'), true);
@@ -174,4 +185,100 @@ exampleModal.addEventListener('show.bs.modal', function (event) {
 
 })
 
+var searchInputField = document.getElementById("searchinputfield")
+searchInputField.addEventListener("keyup", function(e) {
+    if (e.key === 'Enter') {
+        e.preventDefault();
+        document.getElementById("searchbtn").click();
+    }
+});
 
+var searchBtn = document.getElementById("searchbtn")
+searchBtn.addEventListener("click", function(ev) {
+
+    ev.preventDefault();
+
+    if (IsEmpty(searchInputField.value)){
+        alert("Search query should not be empty!")
+    } else {
+        fetch(apiurl + "/search?query=" + searchInputField.value)
+        .then((response) => {
+            return response.json()
+        })
+        .then((data) => {
+            // Work with JSON data here
+            if (data == null) {
+                todosObj = [];
+            } else {
+                todosObj = data
+            }
+
+            let newTododElement = document.getElementById("newTodo");
+            newTododElement.style.display = "none";
+
+            let searchResultElm = document.getElementById("searchResultDiv");
+            let activeTodosElm = document.getElementById("activeTodos");
+            let completedTodosElm = document.getElementById("completedTodos");
+
+            let searchcount = 0;
+            if(todosObj["activetodos"] && todosObj['activetodos'] != null) {
+                searchcount += todosObj['activetodos'].length;
+            }
+            if(todosObj["completedtodos"] && todosObj['completedtodos'] != null) {
+                searchcount += todosObj['completedtodos'].length;
+            }
+            searchResultElm.innerHTML = `<p class="lead"><b>${searchcount} results found.</b> <a style="color:#0790D1"; href="/"> Return to main menu</a></p>`;
+
+
+            activeTodosElm.innerHTML = ``;
+            completedTodosElm.innerHTML = ``;
+            
+
+            let html = "";
+            if(todosObj["activetodos"])
+            todosObj["activetodos"].reverse().forEach(function (element, index) {
+                html += `
+                <div class="todo">
+                    <p class="todo-date text-secondary mx-1"><b>Created on:</b> ${element.date}</p>
+                    <h3 class="todo-title m-1"> ${element.title} </h3>
+                    <p class="todo-text lead mx-1 mt-1 mb-3"> ${element.text} </p>
+                    <button id="${element.id}" type="button" class="btn todo-btn btn-primary m-1" data-bs-toggle="modal" data-bs-target="#exampleModal"
+                    data-bs-whatever="${element.title}" title-data="${element.title}" text-data="${element.text}" id-data=${element.id}>Edit Todo</button>
+                    <button id="${element.id}"onclick="markcompleted(this.id)" class="btn todo-btn btn-success m-1">Mark as completed</button>
+                    <button id="${element.id}"onclick="deleteTodo(this.id)" class="btn todo-btn btn-danger m-1">Delete Todo</button>
+                </div>
+                    `;
+            });
+            
+            if (todosObj['activetodos'] != null && todosObj['activetodos'].length != 0) {
+                activeTodosElm.innerHTML = html;
+            } else {
+                activeTodosElm.innerHTML = `<p class="lead px-1">No todos found!</p>`;
+            }
+
+            html = "";
+            if(todosObj["completedtodos"])
+            todosObj["completedtodos"].reverse().forEach(function (element, index) {
+                html += `
+                <div class="todo">
+                    <p class="todo-date text-secondary"><b>Created on</b> ${element.date}</p>
+                    <h4 class="todo-title"> ${element.title} </h4>
+                    <p class="todo-text lead"> ${element.text}</p>
+                    <button id="${element.id}"onclick="deleteTodo(this.id)" class="btn todo-btn btn-danger m-1">Delete Todo</button>
+                </div>
+                    `;
+            });
+            
+            if (todosObj['completedtodos'] != null && todosObj['completedtodos'].length != 0) {
+                completedTodosElm.innerHTML = html;
+            } else {
+                completedTodosElm.innerHTML = `<p class="lead px-1">No todos found!</p>`;
+            }
+
+        })
+        .catch((err) => {
+            console.log(err)
+        })
+    }
+
+})
